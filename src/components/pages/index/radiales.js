09179c44877bcc2a6fa38a90b76981ff0b3a5ed0
Efrain Colque Calizaya <efraincolquecalizaya@gmail.com>
2025-07-23 07:15:13 -0400

    add: Subida de archivos locales

--- src/components/pages/index/radiales.js
+++ src/components/pages/index/radiales.js
@@ -2,22 +2,22 @@
 export const radiales = [
   {
     titulo: 'Cuña 1: Información general',
-    imagen: 'https://placehold.co/300x200',
+    imagen: 'https://www.fundacionhechoenbolivia.com/fundacionhb/wp-content/uploads/2021/07/object1092908693.gif',
     audio: 'http://www.fundacionhechoenbolivia.com/fundacionhb/wp-content/uploads/2021/07/Uno1.mp3'
   },
   {
     titulo: 'Cuña 2: Servicios disponibles',
-    imagen: 'https://placehold.co/150x150?text=Cuña+2',
+    imagen: 'https://www.fundacionhechoenbolivia.com/fundacionhb/wp-content/uploads/2021/07/object1092908693.gif',
     audio: 'http://www.fundacionhechoenbolivia.com/fundacionhb/wp-content/uploads/2021/07/Dos1.mp3'
   },
   {
     titulo: 'Cuña 3: Llamado a la acción',
-    imagen: '',
+    imagen: 'https://www.fundacionhechoenbolivia.com/fundacionhb/wp-content/uploads/2021/07/object1092908693.gif',
     audio: 'http://www.fundacionhechoenbolivia.com/fundacionhb/wp-content/uploads/2021/07/Tres1.mp3'
   },
   {
     titulo: 'Cuña 4: Información adicional',
-    imagen: '',
+    imagen: 'https://www.fundacionhechoenbolivia.com/fundacionhb/wp-content/uploads/2021/07/object1092908693.gif',
     audio: 'http://www.fundacionhechoenbolivia.com/fundacionhb/wp-content/uploads/2021/07/Cuatro1.mp3'
   }
 ];
